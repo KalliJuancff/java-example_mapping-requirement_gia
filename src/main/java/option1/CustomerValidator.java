ca835e1be38951d390a651072cb9c36bf38fd232
Juan Carlos Fidalgo Fernández <juancfoofn@yahoo.com>
2024-10-28 18:36:55 +0100

    refactor(SUT): simplify return code from 'validate' method of CustomerValidator class

--- src/main/java/option1/CustomerValidator.java
+++ src/main/java/option1/CustomerValidator.java
@@ -2,7 +2,6 @@ package option1;
 
 public class CustomerValidator {
     public boolean validate(Customer customer) {
-        return (customer.hasAddress() || customer.hasMobilePhoneNumber()) && (customer.hasMobilePhoneNumber() || customer.hasWorkPhoneNumber());
+        return (customer.hasWorkPhoneNumber() && customer.hasAddress()) || customer.hasMobilePhoneNumber();
     }
-
 }
