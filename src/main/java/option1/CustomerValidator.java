fc97dccbb1e7d77f9aea7f68cc8444d063930f66
Juan Carlos Fidalgo Fernández <juancfoofn@yahoo.com>
2024-10-28 18:33:38 +0100

    refactor(SUT): consolidate the multiple 'if..then'

--- src/main/java/option1/CustomerValidator.java
+++ src/main/java/option1/CustomerValidator.java
@@ -2,15 +2,7 @@ package option1;
 
 public class CustomerValidator {
     public boolean validate(Customer customer) {
-        if (!customer.hasAddress() && !customer.hasMobilePhoneNumber()) {
-            return false;
-        }
-
-        if (!customer.hasMobilePhoneNumber() && !customer.hasWorkPhoneNumber()) {
-            return false;
-        }
-
-        return true;
+        return (customer.hasAddress() || customer.hasMobilePhoneNumber()) && (customer.hasMobilePhoneNumber() || customer.hasWorkPhoneNumber());
     }
 
 }
